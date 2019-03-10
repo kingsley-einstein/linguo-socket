ecc098f9ccccd9db432360dd36db420ddb1394e9
Kingsley Victor <javaprodigy56@gmail.com>
2019-03-10 23:31:51 +0100

    Another commit. Yay!

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+io.origins("*:*");
+
 io.on('connection', (socket) => {
     console.log(`Connected to socket with id ${socket.id}`);
     socket.on('SEND_MESSAGE', (data) => {
