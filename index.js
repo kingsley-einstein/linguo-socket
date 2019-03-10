e36311646eff7e4803e3d90403c5cee974df0854
Kingsley Victor <javaprodigy56@gmail.com>
2019-03-10 23:23:29 +0100

    Another commit. Yay!

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const io = require('socket.io')(server);
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -9,6 +8,7 @@ app.use((req, res, next) => {
 });
 
 const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
     console.log(`Connected to socket with id ${socket.id}`);
