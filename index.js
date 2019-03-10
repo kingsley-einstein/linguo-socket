ccde0036580628acc8772243d67763858babf250
Kingsley Victor <javaprodigy56@gmail.com>
2019-03-10 23:16:33 +0100

    Another commit. yea. I thought so.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
 io.on('connection', (socket) => {
     console.log(`Connected to socket with id ${socket.id}`);
     socket.on('SEND_MESSAGE', (data) => {
@@ -10,6 +16,7 @@ io.on('connection', (socket) => {
         io.emit('MESSAGE', data);
     });
 });
+
 
 server.listen(4800, () => {
     console.log('Server running on port 4800');
